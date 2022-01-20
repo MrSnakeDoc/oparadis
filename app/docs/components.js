5de8fe797f06570871f52b4eafd8b29a6ca28ad0
MrSnakeDoc <selabbadi@gmail.com>
2022-01-20 17:42:30 +0100

    Revert "documentation almost done"
    This reverts commit b9d1c39874b7d8331a1b674e774616cbc9aa7c14.

--- app/docs/components.js
+++ app/docs/components.js
@@ -463,27 +463,6 @@ module.exports = {
 					house_id: 22,
 				},
 			},
-			Country: {
-				type: "object",
-				required: [],
-				properties: {
-					id: {
-						type: "integer",
-						description: "The auto-generated id",
-						example: 1,
-					},
-					country: {
-						type: "string",
-						description: "The name of the country",
-						required: true,
-						example: "France",
-					},
-				},
-				example: {
-					id: 5,
-					country: "France",
-				},
-			},
 		},
 	},
 };
