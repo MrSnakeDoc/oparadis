d0749a24bddd31fbd2ae65ff88f6c605e5783d8e
MrSnakeDoc <selabbadi@gmail.com>
2022-01-30 16:44:13 +0100

    console.log deleted

--- __tests__/customer_sql.spec.js
+++ __tests__/customer_sql.spec.js
@@ -7,25 +7,21 @@ describe("Test route GET /customers", () => {
 		const response = await request(app).get("/customers");
 		expect(response.statusCode).toBe(200);
 	});
+
 	test("should return an array in response.body", async () => {
 		const response = await request(app).get("/customers");
 		expect(typeof response.body).toBe("object");
 	});
+
 	test("should return an array of objects in response.body", async () => {
 		const response = await request(app).get("/customers");
-		console.log(response.body[0].isAdmin);
-		console.log(typeof response.body[0].isAdmin);
 		expect(typeof response.body[0].id).toBe("number");
 		expect(typeof response.body[0].email).toBe("string");
 		expect(typeof response.body[0].firstname).toBe("string");
 		expect(typeof response.body[0].lastname).toBe("string");
 		expect(typeof response.body[0].pseudo).toBe("string");
 		expect(typeof response.body[0].phone_number).toBe("string");
-		expect(typeof response.body[0].avatar).toBeOneOf([
-			"string",
-			null,
-			"object",
-		]);
+		expect(typeof response.body[0].avatar).toBeOneOf(["string", "object"]);
 		expect(typeof response.body[0].created_at).toBe("string");
 		expect(typeof response.body[0].updated_at).toBe("string");
 	});
