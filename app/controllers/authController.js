279a7796c95748ef88a5f638882b5bbc179b4412
MrSnakeDoc <selabbadi@gmail.com>
2022-01-31 15:26:33 +0100

    deleted a console.log

--- app/controllers/authController.js
+++ app/controllers/authController.js
@@ -61,6 +61,7 @@ module.exports = {
 	},
 
 	async refreshToken(req, res) {
+		console.log(req.headers);
 		try {
 			const ref_token =
 				req.headers.authorization && req.headers.authorization.split(" ")[1];
