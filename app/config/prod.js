524dc6b601f12947f614d4a46002a1daa4683939
MrSnakeDoc <selabbadi@gmail.com>
2022-01-31 14:11:37 +0100

    testing online

--- app/config/prod.js
+++ app/config/prod.js
@@ -5,5 +5,5 @@ module.exports = {
 	redis_url: process.env.REDISCLOUD_URL,
 	jwt_secret: process.env.JWT_SECRET,
 	refresh_jwt_secret: process.env.REFRESH_JWT_SECRET,
-	salt: process.env.SALT || 20,
+	salt: process.env.SALT || 10,
 };
