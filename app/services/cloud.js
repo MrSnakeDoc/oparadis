7d69c9e7157269cd4dfa030c4dece50692ebd91a
MrSnakeDoc <selabbadi@gmail.com>
2022-02-09 17:30:53 +0100

    photo resize

--- app/services/cloud.js
+++ app/services/cloud.js
@@ -1,21 +1,37 @@
-const { cloud_name,  api_key, api_secret} = require('../config');
-const cloudinary = require('cloudinary');
-cloudinary.config({cloud_name, api_key, api_secret});
+const { cloud_name, api_key, api_secret } = require("../config");
+const cloudinary = require("cloudinary");
+cloudinary.config({ cloud_name, api_key, api_secret });
 
 module.exports = {
-  cloudCreatePhoto: async (data) => {
-    cloudinary.image(data);
-    const result = await cloudinary.uploader.upload(data, {width: 800, height: 600, crop: "limit"});
-    return result.url;		
-  },
-  cloudCreateAvatar: async (data) => {
-    cloudinary.image(data);
-    const result = await cloudinary.uploader.upload(data, {width: 200, height: 200, radius: "max", crop: "fill"});
-    return result.url;		
-  },
-  cloudDelete: async (data) => {
-    const key = data.split('/')[data.split('/').length-1].split('.')[0];
-    await cloudinary.uploader.destroy(key);
-    return;
-  },
+	cloudCreatePhoto: async (data) => {
+		cloudinary.image(data);
+		const result = await cloudinary.uploader.upload(data);
+		result.url = result.url
+			.split("https://res.cloudinary.com/oparadis/image/upload/")
+			.splice(1, 1)
+			.map(
+				(x) =>
+					`https://res.cloudinary.com/oparadis/image/upload/c_scale,w_1280/${x}`
+			)
+			.join();
+		return result.url;
+	},
+	cloudCreateAvatar: async (data) => {
+		cloudinary.image(data);
+		const result = await cloudinary.uploader.upload(data);
+		result.url = result.url
+			.split("https://res.cloudinary.com/oparadis/image/upload/")
+			.splice(1, 1)
+			.map(
+				(x) =>
+					`https://res.cloudinary.com/oparadis/image/upload/c_scale,w_200,c_fill,r_max//${x}`
+			)
+			.join();
+		return result.url;
+	},
+	cloudDelete: async (data) => {
+		const key = data.split("/")[data.split("/").length - 1].split(".")[0];
+		await cloudinary.uploader.destroy(key);
+		return;
+	},
 };
