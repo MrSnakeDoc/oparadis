3cb631fd97d70fec1675dbd2530c44182271813a
Michael Lemaire10 <leahcim60@outlook.fr>
2022-02-10 14:19:50 +0100

    modif schema limit charactere

--- app/schemas/photoSchema.js
+++ app/schemas/photoSchema.js
@@ -4,13 +4,13 @@ module.exports = {
 	add_photo: Joi.object({
 		photo: Joi.string().required().error(new Error('photo: This field is required')),
         customer_id: Joi.number().integer().required().error(new Error('customer_id: This field is required, Expected type integer')),
-		Validation: Joi.boolean().error(new Error('Validation: Expected type boolean')),
+		// Validation: Joi.boolean().error(new Error('Validation: Expected type boolean')),
 	}),
 
 	update_photo: Joi.object({
 		photo: Joi.string().required().error(new Error('photo: This field is required')),
 		url: Joi.string().required().error(new Error('url: This field is required, url')),
         customer_id: Joi.number().integer().required().error(new Error('customer_id: This field is required, Expected type integer')),
-		Validation: Joi.boolean().error(new Error('Validation: Expected type boolean')),
+		// Validation: Joi.boolean().error(new Error('Validation: Expected type boolean')),
 	}),
 };
--- app/schemas/plantSchema.js
+++ app/schemas/plantSchema.js
@@ -2,19 +2,19 @@ const Joi = require("joi");
 
 module.exports = {
 	add_plant: Joi.object({
-		type: Joi.string().max(20).required().error(new Error('type: This field is required, characters limit up to 20')),
+		type: Joi.string().max(100).required().error(new Error('type: This field is required, characters limit up to 100')),
 		notes: Joi.string().max(300).error(new Error('notes: Characters limit up to 300')),
 		photo: Joi.string().empty(""),
         customer_id: Joi.number().integer().error(new Error('customer_id: Expected type integer')),
-		Validation: Joi.boolean().error(new Error('Validation: Expected type boolean')),
+		// Validation: Joi.boolean().error(new Error('Validation: Expected type boolean')),
 	}),
 
 	update_plant: Joi.object({
-		type: Joi.string().max(30).required().error(new Error('type: This field is required, characters limit up to 30')),
+		type: Joi.string().max(100).required().error(new Error('type: This field is required, characters limit up to 100')),
 		notes: Joi.string().max(300).error(new Error('notes: Characters limit up to 300')),
 		photo: Joi.string().empty(""),
 		url: Joi.string().empty(""),
         customer_id: Joi.number().integer().error(new Error('customer_id: Expected type integer')),
-		Validation: Joi.boolean().error(new Error('Validation: Expected type boolean')),
+		// Validation: Joi.boolean().error(new Error('Validation: Expected type boolean')),
 	}),
 };
